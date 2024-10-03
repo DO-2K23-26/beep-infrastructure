684e65b7df0988393af8c84e242c86e1e9f2a717
hugoPonthieu <hugo.ponthieu@etu.umontpellier.fr>
2024-10-03 18:47:35 +0200

    typo

--- services/harbor.tf
+++ services/harbor.tf
@@ -84,8 +84,8 @@ resource "helm_release" "harbor" {
       "oidc_endpoint"      = "https://github.com/login/oauth/authorize"
       "oidc_groups_claim"  = "beep"
       "oidc_admin_group"   = "beep"
-      "oidc_client_id"     = var.gh_client_id_harbor
-      "oidc_client_secret" = var.gh_client_secret_harbor
+      "oidc_client_id"     = "${var.gh_client_id_harbor}"
+      "oidc_client_secret" = "${var.gh_client_secret_harbor}"
       "oidc_scope"         = "openid,email,profile"
       "oidc_verify_cert"   = "false"
       "oidc_auto_onboard"  = "true"

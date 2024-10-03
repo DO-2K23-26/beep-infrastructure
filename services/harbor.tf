a54bcd31da552a914e295d79f5f70559d1f02cfc
hugoPonthieu <hugo.ponthieu@etu.umontpellier.fr>
2024-10-03 23:00:31 +0200

    escaped + eot

--- services/harbor.tf
+++ services/harbor.tf
@@ -78,7 +78,20 @@ resource "helm_release" "harbor" {
   }
   set {
     name  = "core.extraEnvVars[0].value"
-    value = "{\"auth_mode\" = \"oidc_auth\" \"oidc_name\" = \"Github\" \"oidc_endpoint\" = \"https://github.com/login/oauth/authorize\" \"oidc_groups_claim\" = \"beep\" \"oidc_admin_group\" = \"beep\" \"oidc_client_id\" = \"${var.gh_client_id_harbor}\" \"oidc_client_secret\" = \"${var.gh_client_secret_harbor}\" \"oidc_scope\" = \"openid\" \"oidc_verify_cert\" = false \"oidc_auto_onboard\" = true}"
-
+    value = <<-EOT
+  {
+    \"auth_mode\": \"oidc_auth\",
+    \"oidc_name\": \"Github\",
+    \"oidc_endpoint\": \"https://github.com/login/oauth/authorize\",
+    \"oidc_groups_claim\": \"beep\",
+    \"oidc_admin_group\": \"beep\",
+    \"oidc_client_id\": \"${var.gh_client_id_harbor}\",
+    \"oidc_client_secret\": \"${var.gh_client_secret_harbor}\",
+    \"oidc_scope\": \"openid\",
+    \"oidc_verify_cert\": false,
+    \"oidc_auto_onboard\": true
   }
+  EOT
+  }
+
 }

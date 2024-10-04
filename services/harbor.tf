ac47331e0f6421edcd7a62a44c4a1cc62f9672bc
hugoPonthieu <hugo.ponthieu@etu.umontpellier.fr>
2024-10-04 02:18:50 +0200

    delete harbor

--- services/harbor.tf
+++ services/harbor.tf
@@ -21,19 +21,19 @@ resource "helm_release" "postgres-harbor" {
     value = "registry"
   }
 }
-resource "helm_release" "harbor" {
-  depends_on = [helm_release.postgres-harbor, kubectl_manifest.clusterissuer_letsencrypt_prod]
-  name       = "harbor"
-  repository = "https://charts.bitnami.com/bitnami"
-  chart      = "harbor"
-  namespace  = "harbor"
+# resource "helm_release" "harbor" {
+#   depends_on = [helm_release.postgres-harbor, kubectl_manifest.clusterissuer_letsencrypt_prod]
+#   name       = "harbor"
+#   repository = "https://charts.bitnami.com/bitnami"
+#   chart      = "harbor"
+#   namespace  = "harbor"
 
-  values = [
-    templatefile("values/harbor.yaml.tpl", {
-      domain_name             = var.domain_name
-      gh_client_id_harbor     = var.gh_client_id_harbor
-      gh_client_secret_harbor = var.gh_client_secret_harbor
-    })
-  ]
+#   values = [
+#     templatefile("values/harbor.yaml.tpl", {
+#       domain_name             = var.domain_name
+#       gh_client_id_harbor     = var.gh_client_id_harbor
+#       gh_client_secret_harbor = var.gh_client_secret_harbor
+#     })
+#   ]
 
-}
+# }

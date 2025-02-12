2058582656ba9ccbb821de009ffb2062e80afa2f
duratm <duratmathias@gmail.com>
2025-02-12 14:04:26 +0100

    feat(stunner): change to helm_release

--- services/stunner.tf
+++ services/stunner.tf
@@ -4,52 +4,27 @@ resource "kubernetes_namespace" "stunner_system" {
   }
 }
 
-resource "kubernetes_manifest" "stunner_gateway_operator" {
-  depends_on = [kubernetes_namespace.stunner_system, helm_release.argocd]
-  manifest = {
-    "apiVersion" = "argoproj.io/v1alpha1"
-    "kind"       = "Application"
-    "metadata" = {
-      "name"      = "stunner-gateway-operator"
-      "namespace" = "argocd"
-    }
-    "spec" = {
-      "destination" = {
-        "name"      = ""
-        "namespace" = "stunner-system"
-        "server"    = "https://kubernetes.default.svc"
-      }
-      "source" = {
-        "path"            = ""
-        "repoURL"         = "https://l7mp.io/stunner"
-        "targetRevision"  = "0.21.0"
-        "chart"           = "stunner-gateway-operator"
-        "helm" = {
-          "values" = <<-EOT
-          stunnerGatewayOperator:
-            dataplane:
-              spec:
-                hostNetwork: false
-                enableMetricsEndpoint: true
-          EOT
-        }
-      }
-      "project"    = "default"
-      "syncPolicy" = {
-        "automated" = {
-          "prune"    = true
-          "selfHeal" = true
-        }
-        "syncOptions" = [
-          "CreateNamespace=true",
-          "ServerSideApply=true"
-        ]
-      }
-    }
+resource "helm_release" "stunner_gateway_operator" {
+  depends_on       = [kubernetes_namespace.stunner_system, helm_release.argocd]
+  name             = "stunner-gateway-operator"
+  repository       = "https://l7mp.io/stunner"
+  chart            = "stunner-gateway-operator"
+  namespace        = "stunner-system"
+  version          = "0.21.0"
+  create_namespace = true
+
+  set {
+    name  = "stunnerGatewayOperator.dataplane.spec.hostNetwork"
+    value = "false"
+  }
+
+  set {
+    name  = "stunnerGatewayOperator.dataplane.spec.enableMetricsEndpoint"
+    value = "true"
   }
 }
 
 output "stunner_deployed" {
-  depends_on = [kubernetes_manifest.stunner_gateway_operator]
-  value = true
+  depends_on = [helm_release.stunner_gateway_operator]
+  value      = true
 }
